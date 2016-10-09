d5d0d171059914d16b1ee89ca3c72a1e83f86051
Matt Crowder <mcrowder65@gmail.com>
2016-10-09 17:35:16 -0600

    beta done!

--- example.py
+++ example.py
@@ -7,31 +7,21 @@ arr = sys.argv
 username = arr[1]
 password = arr[2]
 hashTags = arr[3].split(',')
-likesPerDay = arr[4]
-maxLikesForOneTag = arr[5]
-followsPerDay = arr[6]
-unfollowsPerDay = arr[7]
+likesPerDay = int(arr[4])
+maxLikesForOneTag = int(arr[5])
+followsPerDay = int(arr[6])
+unfollowsPerDay = int(arr[7])
 
-# print 'username: ' + username
-x = 0
-for i in range(0, 10000000000000):
-     x = i
-# print 'password: ' + password
-# print 'hashTags: ' + str(hashTags)
-# print 'likesPerDay: ' + likesPerDay
-# print 'maxLikesForOneTag: ' + maxLikesForOneTag
-# print 'followsPerDay: ' + followsPerDay
-# print 'unfollowsPerDay: ' + unfollowsPerDay
-# bot = InstaBot(login=arr[1], password=arr[2],
-#                like_per_day=1000,
-#                comments_per_day=0,
-#                tag_list=tags,
-#                max_like_for_one_tag=1000,
-#                follow_per_day=0,
-#                follow_time=0,
-#                unfollow_per_day=150,
-#                unfollow_break_min=0,
-#                unfollow_break_max=0,
-#                log_mod=0)
+bot = InstaBot(login=username, password=password,
+               like_per_day=likesPerDay,
+               comments_per_day=0,
+               tag_list=hashTags,
+               max_like_for_one_tag=maxLikesForOneTag,
+               follow_per_day=followsPerDay,
+               follow_time=0,
+               unfollow_per_day=unfollowsPerDay,
+               unfollow_break_min=0,
+               unfollow_break_max=0,
+               log_mod=0)
 
-# bot.new_auto_mod()
+bot.new_auto_mod()
